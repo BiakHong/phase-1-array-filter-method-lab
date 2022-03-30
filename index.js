df6f66335b290611f344554b9a8f9cc2b08f3f5a
Lian <biakhong@gmail.com>
2022-03-30 14:07:09 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(nameArr, namesStr){
+    const result = nameArr.filter(word => word.toLowerCase() === namesStr.toLowerCase());
+    return result;
+
+}
+function fuzzyMatch(driverNames, aLetter){
+    const result = driverNames.filter(word => word.charAt(0).toUpperCase() === aLetter.charAt(0).toUpperCase());
+    return result;
+
+
+}
+function matchName(driver, objString ){
+    const result = driver.filter(word => word.name === objString);
+    return result;
+}
